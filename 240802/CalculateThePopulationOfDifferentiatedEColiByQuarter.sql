93c873afa3a6a9ec3e598757d29386c2906728ec
jiyoungmerong <jiyoungmerong@gmail.com>
2024-08-02 22:24:49 +0900

    분기별 분화된 대장균의 개체 수 구하기

---
+++ 240802/CalculateThePopulationOfDifferentiatedEColiByQuarter.sql
@@ -0,0 +1,26 @@
+/*
+[문제 설명]
+대장균들은 일정 주기로 분화하며, 분화를 시작한 개체를 부모 개체, 분화가 되어 나온 개체를 자식 개체라고 합니다.
+다음은 실험실에서 배양한 대장균들의 정보를 담은 ECOLI_DATA 테이블입니다.
+ECOLI_DATA 테이블의 구조는 다음과 같으며, ID, PARENT_ID, SIZE_OF_COLONY, DIFFERENTIATION_DATE, GENOTYPE 은
+각각 대장균 개체의 ID, 부모 개체의 ID, 개체의 크기, 분화되어 나온 날짜, 개체의 형질을 나타냅니다.
+
+Column name	            Type	    Nullable
+ID	                    INTEGER	    FALSE
+PARENT_ID	            INTEGER	    TRUE
+SIZE_OF_COLONY	        INTEGER	    FALSE
+DIFFERENTIATION_DATE	DATE	    FALSE
+GENOTYPE	INTEGER	FALSE
+
+최초의 대장균 개체의 PARENT_ID 는 NULL 값입니다.
+*/
+
+SELECT
+    CONCAT(CEIL(MONTH(DIFFERENTIATION_DATE) / 3), 'Q') QUARTER,
+    COUNT(ID) ECOLI_COUNT
+FROM
+    ECOLI_DATA
+GROUP BY
+    QUARTER
+ORDER BY
+    QUARTER
